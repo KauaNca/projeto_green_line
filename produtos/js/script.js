62eb13a0db6b57550fb4b4b6603765e57ad83d42
gabriel-reiss <gabreel47@gmail.com>
2024-09-25 11:21:53 -0300

    ATT

--- produtos/js/script.js
+++ produtos/js/script.js
@@ -50,15 +50,25 @@ function atualizarCarrinho() {
   document.getElementById('carrinho-total').innerHTML = `Valor Total: R$${valorTotal.toFixed(2)}`;
 }
 
+// Função para limpar o carrinho
+function limparCarrinho() {
+  console.log('Limpeza do carrinho iniciada');
+  // Limpa o carrinho
+  carrinho = [];
+  console.log('Carrinho limpo');
+  // Atualiza o carrinho na tela
+  atualizarCarrinho();
+}
+
 // Adiciona evento de clique aos botões de comprar
-let comprarButtons = document.getElementsByClassName('agendamento-botao');
+let comprarButtons = document.querySelectorAll('.btn-comprar button');
 for (let i = 0; i < comprarButtons.length; i++) {
   let button = comprarButtons[i];
   button.addEventListener('click', function() {
     // Obtém o produto associado ao botão
     let produto = {
-      nome: button.parentNode.parentNode.querySelector('.body-txt').textContent,
-      preco: parseFloat(button.parentNode.parentNode.querySelector('.preco-servico').textContent.replace('R$', ''))
+      nome: button.parentNode.parentNode.querySelector('h4').textContent,
+      preco: parseFloat(button.parentNode.parentNode.querySelector('p').textContent.replace('$', ''))
     };
     
     // Adiciona o produto ao carrinho
@@ -83,4 +93,30 @@ let limparCarrinhoButton = document.getElementById('limpar-carrinho');
 limparCarrinhoButton.addEventListener('click', function() {
   // Limpa o carrinho
   limparCarrinho();
+});
+
+// Adiciona evento de clique ao botão de comprar no carrinho
+let comprarCarrinhoButton = document.getElementById('comprar-carrinho');
+comprarCarrinhoButton.addEventListener('click', function() {
+  // Realiza a compra do carrinho
+  alert('Compra realizada com sucesso!');
+  
+  // Limpa o carrinho
+  limparCarrinho();
+  
+  // Atualiza o valor total do carrinho para o valor inicial
+  document.getElementById('carrinho-total').innerHTML = 'Valor Total: R$0,00';
+  
+  // Limpa o conteúdo do carrinho
+  let carrinhoElement = document.getElementById('carrinho-produtos');
+  console.log(carrinhoElement); // Verifique se o elemento está sendo encontrado
+  carrinhoElement.innerHTML = '';
+});
+
+// Adiciona evento de clique ao botão de fechar o carrinho
+let fecharCarrinhoButton = document.getElementById('fechar-carrinho');
+fecharCarrinhoButton.addEventListener('click', function() {
+  // Esconde o carrinho
+  let carrinhoElement = document.getElementById('carrinho');
+  carrinhoElement.style.display = 'none';
 });
