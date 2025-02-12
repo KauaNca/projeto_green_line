a903b8752765a3bac0a58d39ee862423737e639e
gabriel-reiss <gabreel47@gmail.com>
2025-02-12 11:18:33 -0300

    desktop

---
+++ desktop/green/src/green/Green.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package green;
+
+/**
+ *
+ * @author Gabriel54274586
+ */
+public class Green {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
