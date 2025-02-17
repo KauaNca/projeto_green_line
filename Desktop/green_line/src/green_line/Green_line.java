72165d70fb6da85cd1b1b788c6c80b6cd49d3ba1
Kauã Nunes <kauanunes2003@gmail.com>
2025-02-17 11:20:48 -0300

    kaua_commit

--- Desktop/green_line/src/green_line/Green_line.java
+++ Desktop/green_line/src/green_line/Green_line.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package green;
+package green_line;
 
 /**
  *
- * @author Gabriel54274586
+ * @author Kaua33500476
  */
-public class Green {
+public class Green_line {
 
     /**
      * @param args the command line arguments
