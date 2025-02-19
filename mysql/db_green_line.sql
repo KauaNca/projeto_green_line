ea1f470572f970f429c83fc713c3d79fc2aa6af5
KauaNca <kauanunes2003@gmail.com>
2025-02-18 22:32:00 -0300

    Update db_green_line.sql

--- mysql/db_green_line.sql
+++ mysql/db_green_line.sql
@@ -5,30 +5,30 @@ CREATE TABLE pessoa (
     id_pessoa INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(40) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE CHECK (email LIKE '%@%.%'),
-    senha VARCHAR(255) NOT NULL,
     telefone VARCHAR(15) NOT NULL,
     cpf_cnpj VARCHAR(18) NOT NULL UNIQUE,
     rg VARCHAR(12) NULL,
     genero ENUM('M', 'F', 'O') NULL,
     idade int null
 );
-
+-- mais flexibilidade
+CREATE TABLE tipo_usuario(
+id_tipo int primary key auto_increment,
+tipo varchar(35) not null
+);
 CREATE TABLE usuario (
     id_usuario INT PRIMARY KEY AUTO_INCREMENT,
     id_pessoa INT NOT NULL UNIQUE, 
     FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa) ON DELETE CASCADE,
     id_tipo_usuario INT NOT NULL, 
     FOREIGN KEY (id_tipo_usuario) REFERENCES tipo_usuario(id_tipo),
+	senha VARCHAR(255) NOT NULL,
     nivel_acesso VARCHAR(15) NOT NULL DEFAULT 'Sem acesso',
     situacao ENUM('A','I') NOT NULL,
     data_hora DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- mais flexibilidade
-CREATE TABLE tipo_usuario(
-id_tipo int primary key auto_increment,
-tipo varchar(35) not null
-);
+
 
 CREATE TABLE acessos (
     id_acesso INT PRIMARY KEY AUTO_INCREMENT, 
@@ -93,12 +93,6 @@ CREATE TABLE subcategorias (
     descricao TEXT NULL
 );
 
-CREATE TABLE imagens (
-    id_imagem INT PRIMARY KEY AUTO_INCREMENT,
-    endereco VARCHAR(255) NOT NULL,
-    id_produto INT NOT NULL,
-    FOREIGN KEY (id_produto) REFERENCES produto(id_produto)
-);
 
 CREATE TABLE produto (
     id_produto INT PRIMARY KEY AUTO_INCREMENT,
@@ -111,6 +105,14 @@ CREATE TABLE produto (
     FOREIGN KEY (id_subcat) REFERENCES subcategorias(id_subcat)
 );
 
+CREATE TABLE imagens (
+    id_imagem INT PRIMARY KEY AUTO_INCREMENT,
+    endereco VARCHAR(255) NOT NULL,
+    id_produto INT NOT NULL,
+    FOREIGN KEY (id_produto) REFERENCES produto(id_produto)
+);
+
+
 CREATE TABLE pedidos_produtos (
     id_pedido INT NOT NULL,
     FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido),
@@ -121,6 +123,30 @@ CREATE TABLE pedidos_produtos (
     desconto DECIMAL(10,2) DEFAULT 0 CHECK (desconto >= 0),
     subtotal DECIMAL(10,2) GENERATED ALWAYS AS ((valor - desconto) * quantidade) STORED,
     cancelado CHAR(1) NOT NULL DEFAULT 'N' CHECK (cancelado IN ('S', 'N', 'P')),
-    quantidade_cancelada INT DEFAULT 0 CHECK (quantidade_cancelada >= 0 AND quantidade_cancelada <= quantidade)
+    quantidade_cancelada INT DEFAULT 0,
+    CONSTRAINT chk_quantidade_cancelada CHECK (quantidade_cancelada >= 0 AND quantidade_cancelada <= quantidade)
 );
 
+DROP DATABASE green_line;
+
+-- INSERINDO DADOS
+
+INSERT INTO pessoa (nome, email,  telefone, cpf_cnpj, rg, genero, idade) VALUES 
+('Kauã', 'kaua@example.com',  '1234567890', '123.456.789-09', '12.345.678-9', 'M', 21),
+('Gabriel', 'gabriel@example.com',  '0987654321', '987.654.321-00', '98.765.432-1', 'M', 25),
+('Edenilson', 'edenilson@example.com',  '1122334455', '112.233.445-67', '11.223.344-5', 'M', 30);
+
+INSERT INTO tipo_usuario (tipo) VALUES 
+('Administrador'), 
+('Usuário Comum');
+
+INSERT INTO usuario (id_pessoa, id_tipo_usuario, senha, nivel_acesso, situacao) VALUES 
+((SELECT id_pessoa FROM pessoa WHERE nome = 'Kauã'), 1, '123','Com acesso', 'A'),
+((SELECT id_pessoa FROM pessoa WHERE nome = 'Gabriel'), 1, '123','Com acesso', 'A'),
+((SELECT id_pessoa FROM pessoa WHERE nome = 'Edenilson'), 1, '123','Com acesso', 'A');
+
+SELECT * FROM tipo_usuario;
+
+SELECT nome FROM usuario INNER JOIN pessoa ON pessoa.id_pessoa = usuario.id_pessoa WHERE id_usuario = 1;
+
+SELECT id_usuario, id_tipo_usuario, nivel_acesso FROM usuario INNER JOIN pessoa ON pessoa.id_pessoa = usuario.id_pessoa WHERE nome = 'Kauã'AND senha = 123;
