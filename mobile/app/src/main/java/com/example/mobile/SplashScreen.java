6db4b6d8a3bc63bb7fab3e93314bc94e5f8d35fe
Kauã Nunes <kauanunes2003@gmail.com>
2025-03-27 11:27:07 -0300

    atualizacao

--- mobile/app/src/main/java/com/example/mobile/SplashScreen.java
+++ mobile/app/src/main/java/com/example/mobile/SplashScreen.java
@@ -1,11 +1,15 @@
 package com.example.mobile;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.Window;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,6 +26,8 @@ public class SplashScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
