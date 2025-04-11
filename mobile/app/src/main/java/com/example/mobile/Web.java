d5bdfb2e426a5b236ac5b2b21f37f33f13946624
Kauã Nunes <kauanunes2003@gmail.com>
2025-04-10 22:36:07 -0300

    webview

---
+++ mobile/app/src/main/java/com/example/mobile/Web.java
@@ -0,0 +1,31 @@
+package com.example.mobile;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Web extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_web);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        webView = findViewById(R.id.viewWeb);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("C:\\Users\\Kaua Nunes\\Desktop\\projeto_green_line\\web_mobile/sobre.html");
+    }
+}
